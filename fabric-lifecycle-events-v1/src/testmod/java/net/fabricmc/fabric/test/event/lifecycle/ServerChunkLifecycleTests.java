230071a0efb139ba7376c1621b2594566098c786
DennisOchulor <113277083+DennisOchulor@users.noreply.github.com>
2025-05-02 19:45:20 +0800

    Add ServerChunkEvents.CHUNK_LEVEL_TYPE_CHANGE (#4541)
    * Add ServerChunkEvents.CHUNK_LEVEL_TYPE_CHANGE
    
    * Fix naming
    
    * Amend ServerChunkEvents#CHUNK_UNLOAD documentation to clarify server chunk unload timing.
    
    * License header and checkstyle
    
    * Call event for intermediate level type demotion and call event when chunk's future is actually completed.
    
    * Only fire the event async if not already on server thread
    
    * When the event fires to promote ChunkLevelType, ensure the old and new levelTypes are actually sequential
    
    * If chunk level type got demoted before promotion event fires, then don't fire it.
    
    * Update documentation for the event.
    
    * test cleanup and use for loop for decreaseLevel
    
    * more tests
    
    * it mostly works except BLOCK_TICKING->ENTITY_TICKING is still firing off-thread
    
    * BLOCK_TICJING -> ENTITY_TICKING now fires on server thread
    
    * checkstyle
    
    * some javadoc
    
    * BLOCK_TICKING->ENTITY_TICKING fires earlier now, and enhance tests
    
    * checkstyle
    
    * review comments
    
    * handle exception instead of letting completeablefuture swallow it
    
    * complete rewrite
    
    * improve tests and some doc edits
    
    * add test to verify all chunks in all worlds are INACCESSIBLE when server stopped.
    
    * javadoc changes from review
    
    * use @Override instead of @Unique

--- fabric-lifecycle-events-v1/src/testmod/java/net/fabricmc/fabric/test/event/lifecycle/ServerChunkLifecycleTests.java
+++ fabric-lifecycle-events-v1/src/testmod/java/net/fabricmc/fabric/test/event/lifecycle/ServerChunkLifecycleTests.java
@@ -17,22 +17,30 @@
 package net.fabricmc.fabric.test.event.lifecycle;
 
 import com.mojang.logging.LogUtils;
+import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
+import it.unimi.dsi.fastutil.objects.Object2ObjectMap;
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import org.slf4j.Logger;
 
+import net.minecraft.server.world.ChunkLevelType;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.ChunkPos;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerChunkEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 
 public final class ServerChunkLifecycleTests implements ModInitializer {
 	private static final Logger LOGGER = LogUtils.getLogger();
+	private record ChunkLevelTypeEvent(ChunkLevelType oldLevelType, ChunkLevelType newLevelType) { }
 
 	@Override
 	public void onInitialize() {
 		setupChunkGenerateTest();
+		setupChunkLevelTypeChangeTest();
 	}
 
 	/**
@@ -53,6 +61,80 @@ public final class ServerChunkLifecycleTests implements ModInitializer {
 
 		ServerChunkEvents.CHUNK_GENERATE.register((world, chunk) -> {
 			generated.mergeInt(world.getRegistryKey().getValue(), 1, Integer::sum);
+		});
+	}
+
+	/**
+	 * While the world is loading in, this will log a few times.
+	 * Once all chunks within (and just outside) simulation distance have loaded in, logging stops.
+	 * Moving around within the same chunk (use F3+G) should not log anything.
+	 * Moving into another chunk should trigger some logs.
+	 */
+	private static void setupChunkLevelTypeChangeTest() {
+		final Object2ObjectMap<Identifier, Object2IntMap<ChunkLevelType>> worldsChunkLevelEvents = new Object2ObjectOpenHashMap<>();
+		final Object2ObjectMap<Identifier, Long2ObjectOpenHashMap<ChunkLevelTypeEvent>> worldsChunkLevelTypeTracker = new Object2ObjectOpenHashMap<>();
+
+		ServerChunkEvents.CHUNK_LEVEL_TYPE_CHANGE.register((world, worldChunk, oldLevelType, newLevelType) -> {
+			final Identifier worldKey = world.getRegistryKey().getValue();
+
+			if (!world.getServer().isOnThread()) {
+				world.getServer().stop(false); // make sure the server actually "crashes", the throw below will just log the error.
+				throw new AssertionError("CHUNK_LEVEL_TYPE_CHANGE for " + worldKey + " NOT ON SERVER THREAD: " + oldLevelType + "->" + newLevelType);
+			}
+
+			if (worldChunk == null) {
+				throw new AssertionError("CHUNK_LEVEL_TYPE_CHANGE for " + worldKey + " NULL WORLD CHUNK: " + oldLevelType + "->" + newLevelType);
+			}
+
+			final ChunkPos chunkPos = worldChunk.getPos();
+
+			if (Math.abs(oldLevelType.ordinal() - newLevelType.ordinal()) != 1) { // check if the levelTypes are actually sequential, also ensures levelTypes are not the same
+				throw new AssertionError("CHUNK_LEVEL_TYPE_CHANGE for " + worldKey + " " + chunkPos + " NOT SEQUENTIAL: " + oldLevelType + "->" + newLevelType);
+			}
+
+			ChunkLevelTypeEvent prevEvent = worldsChunkLevelTypeTracker.computeIfAbsent(worldKey, obj -> new Long2ObjectOpenHashMap<>()).computeIfAbsent(chunkPos.toLong(), l -> new ChunkLevelTypeEvent(ChunkLevelType.INACCESSIBLE, ChunkLevelType.INACCESSIBLE));
+
+			if (prevEvent.newLevelType() != oldLevelType) { // check if newLevelType from the previous event == oldLevelType for this current event. Catches any out-of-sync firing issues.
+				throw new AssertionError("CHUNK_LEVEL_TYPE_CHANGE for " + worldKey + " " + chunkPos + " PREVIOUS_EVENT: " + prevEvent.oldLevelType() + "->" + prevEvent.newLevelType() + " / CURRENT_EVENT: " + oldLevelType + "->" + newLevelType);
+			}
+
+			worldsChunkLevelTypeTracker.get(worldKey).put(chunkPos.toLong(), new ChunkLevelTypeEvent(oldLevelType, newLevelType));
+			worldsChunkLevelEvents.computeIfAbsent(worldKey, obj -> new Object2IntOpenHashMap<>()).mergeInt(newLevelType, 1, Integer::sum);
+		});
+
+		ServerTickEvents.END_WORLD_TICK.register(world -> {
+			if (world.getTime() % 20 == 0) { // limit to 1 per second
+				Object2IntMap<ChunkLevelType> levelTypes = worldsChunkLevelEvents.get(world.getRegistryKey().getValue());
+
+				if (levelTypes != null && !levelTypes.isEmpty()) {
+					StringBuilder sb = new StringBuilder(world.getRegistryKey().getValue() + " ");
+					// Logs the number of level type changes for each ChunkLevelType, only logs the newLevelType
+					levelTypes.forEach((newLevelType, numOfEvents) -> sb.append(newLevelType).append(": ").append(numOfEvents).append(", "));
+					LOGGER.info(sb.toString());
+					levelTypes.clear();
+				}
+			}
+		});
+
+		ServerLifecycleEvents.SERVER_STOPPED.register(server -> {
+			worldsChunkLevelTypeTracker.forEach((id, chunks) -> {
+				final Object2IntMap<ChunkLevelType> totals = new Object2IntOpenHashMap<>();
+				chunks.forEach((chunkPos, chunkLevelTypeEvent) -> {
+					totals.mergeInt(chunkLevelTypeEvent.newLevelType(), 1, Integer::sum);
+				});
+
+				if (totals.containsKey(ChunkLevelType.FULL) || totals.containsKey(ChunkLevelType.BLOCK_TICKING) || totals.containsKey(ChunkLevelType.ENTITY_TICKING)) {
+					StringBuilder sb = new StringBuilder("CHUNK_LEVEL_TYPE_CHANGE expected all chunks to be INACCESSIBLE for " + id + ", instead got ");
+					totals.forEach((chunkLevelType, finalTotal) -> {
+						sb.append(chunkLevelType).append(": ").append(finalTotal);
+					});
+					LOGGER.error(sb.toString());
+				}
+			});
+
+			// clear everything otherwise it may trip the test incorrectly when you open another world
+			worldsChunkLevelEvents.clear();
+			worldsChunkLevelTypeTracker.clear();
 		});
 	}
 }
